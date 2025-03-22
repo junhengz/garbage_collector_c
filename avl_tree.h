1177025d5ca6346bab262c38d268b282f9ade8ba
qtao <qtao@arista.com>
2025-03-22 13:45:07 -0700

    add address hi to node

--- avl_tree.h
+++ avl_tree.h
@@ -1,6 +1,6 @@
 #include "node.h"
 
 void printTree(struct Node *root, int space);
-struct Node *avl_insert(struct Node *root, void *address);
+struct Node *avl_insert(struct Node *root, void *address, size_t size);
 struct Node *deleteNode(struct Node *root, void *address);
 struct Node *avl_search(struct Node *root, void *address);
--- node.h
+++ node.h
@@ -3,8 +3,9 @@
 
 struct Node {
     void *address;             // Address of the alloc object
+    size_t size;               // Sizeof alloc object
     int height;                // Height of node
     struct Node *left, *right; // left and right child of node
 } Node;
 
-struct Node* newNode(void *address);
+struct Node* newNode(void *address, size_t size);
